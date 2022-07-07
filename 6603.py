51fb1d7612bceedc2a329a673f6b5675ebf83953
chaenLIM <limce21@naver.com>
2022-07-07 14:46:53 +0900

    로또

--- 6603.py
+++ 6603.py
@@ -1,27 +1,15 @@
+from itertools import combinations
 import sys
-
-
-def lotto(S, isused, k, lst):
-    global cnt
-    if len(lst) == 6:
-        print(lst)
-    for i in range(k):
-        if isused[i]:
-            continue
-        else:
-            isused[i] = 1
-            lst.append(S[i])
-            lotto(isused, i)
-            isused[i] = 0
-
 
 while(1):
     inputt = list(map(int, sys.stdin.readline().split()))
     if inputt[0] == 0:
         break
     k = inputt[0]
-    S = input[1:]
-    cnt = 0
-    isused = [0] * k
-    lst = []
-    lotto(S, isused, 1, lst)
+    S = inputt[1:]
+    lst = list(combinations(S, 6))
+    for i in lst:
+        for j in range(6):
+            print(i[j], end=" ")
+        print()
+    print()
